562ff246d625488fdb1ac75a56222edc095e7f62
J-Plum <bosswjdwo@gmail.com>
2023-03-22 18:25:46 +0900

    [test] date picker , full calendar 테스트

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div style={{ width: '45%', color: 'red' }}>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={[
+          {
+            title: '미니 프로젝트',
+            start: '2023-03-22',
+            end: '2023-03-31',
+          },
+          { title: '만우절', start: '2023-04-01' },
+        ]}
+      />
+    </div>
+  );
 }
