fcdd75c6725bfa00622ae5614939fd7101a9185d
J-Plum <bosswjdwo@gmail.com>
2023-03-30 00:13:20 +0900

    [Modify] Main filter 기능 수정

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,7 +9,6 @@ export default function SideBar() {
   const navigate = useNavigate();
 
   const { user } = useService();
-  console.log(user);
 
   return (
     <Container>
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -4,8 +4,18 @@ import Service from '../service/Service';
 
 export const ApiContext = createContext();
 
+export const color = {
+  ANNAUL_COLOR: '#D3D3D3', //휴가
+  DUTY_COLOR: '#FF9AA2', // 당직
+  DEV_DEPT_COLOR: '#B5EAD7', //개발x팀
+  HR_DEPT_COLOR: '#C7CEEA', //인사
+  DESIGN_DEPT_COLOR: '#FFB7B2', //디자인
+};
+
 export function ApiProvider({ children }) {
   const [user, setUser] = useState({});
+  const [selectedUser, setSelectedUser] = useState([]); // 클릭해서 담은 값
+
   const service = new Service();
 
   useEffect(() => {
@@ -13,7 +23,9 @@ export function ApiProvider({ children }) {
   }, []);
 
   return (
-    <ApiContext.Provider value={{ service, user }}>
+    <ApiContext.Provider
+      value={{ service, user, selectedUser, setSelectedUser, color }}
+    >
       {children}
     </ApiContext.Provider>
   );
