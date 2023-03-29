6266e60d9ce4baf3b71a8093f44523099e28cae4
gamangee <gamangee1991@gmail.com>
2023-03-30 00:43:59 +0900

    [Modify] Service.js promise에서 async로 수정

--- src/service/Service.js
+++ src/service/Service.js
@@ -22,24 +22,30 @@ export default class Service {
     return this.client.post(`/account/update/${data.accountId}`, data, config);
   }
 
-  registerSchedule(data) {
-    return this.client
-      .post('/schedule/save', data, config)
-      .then(() => alert('등록 성공'))
-      .catch(error => alert(error));
+  async registerSchedule(data) {
+    try {
+      await this.client.post('/schedule/save', data, config);
+      return alert('등록 성공');
+    } catch (error) {
+      return alert(error);
+    }
   }
 
   async updateSchedule(dataId, data) {
-    return this.client
-      .post(`/schedule/update/${dataId}`, data, config)
-      .then(() => alert('수정 성공'))
-      .catch(error => alert(error));
+    try {
+      await this.client.post(`/schedule/update/${dataId}`, data, config);
+      return alert('수정 성공');
+    } catch (error) {
+      return alert(error);
+    }
   }
 
   async deleteSchedule(data) {
-    return this.client
-      .post(`/schedule/delete/${data.id}`, data, config)
-      .then(() => alert('삭제 성공'))
-      .catch(error => alert(error));
+    try {
+      await this.client.post(`/schedule/delete/${data.id}`, data, config);
+      return alert('삭제 성공');
+    } catch (error) {
+      return alert(error);
+    }
   }
 }
